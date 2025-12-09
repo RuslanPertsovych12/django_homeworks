2b7d1dcbf175989f950735fc3d827c269f84cc56
Ruslan Pertsovych <you@example.com>
2025-12-09 13:43:47 +0200

    homework

--- homework_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HomeworkAppConfig(AppConfig):
-    name = 'homework_app'
--- homework_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('products/', views.products_list, name='products_list'),
-    path('replenish/<int:count>/', views.replenish, name='replenish'),
-]
